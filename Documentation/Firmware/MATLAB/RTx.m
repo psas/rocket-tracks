2f6b818c41605200bd28be28c98b673c24d022bb
JustRob83 <rob@rlgaskell.com>
2014-04-04 13:05:07 -0700

    Added MATLAB scripts, updated RTxController.pdf.

---
+++ Documentation/Firmware/MATLAB/RTx.m
@@ -0,0 +1,6 @@
+p = 3789395
+q = 321.3
+u = 48495
+Cf = 0
+den = [ki*p/I, kp*p/I, (Cf*p + kd*p)/I, (I*p + Cf*u)/I, (I*u + Cf*q)/I, (I*q + Cf)/I, 1]
+num = [ki*p, kp*p + ki*u, kd*p + kp*u + ki*q, kd*u + kp*q + ki, kd*q + kp, kd]
